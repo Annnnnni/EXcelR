a24d837eb68c4ec7fb50f96f211c9d945ff60076
anandtechhh@gmail.com Anand Rathod <anandtechhh@gmail.com>
2023-09-04 13:09:52 +0530

    first commit

---
+++ 1st sept/js.js
@@ -0,0 +1,8 @@
+let array=["anand","baldev","Rathod","anand"]
+let array2=[1,2,3,4,5]
+// array.find("anand")
+array2.sort()
+console.log(array)
+console.log(array2)
+console.log(array.indexOf("anand"))
+console.log(array.lastIndexOf("anand"))
