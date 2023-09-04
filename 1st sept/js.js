1f0ce184835f26daecf603125c67036cc3d31c08
anandtechhh@gmail.com Anand Rathod <anandtechhh@gmail.com>
2023-09-04 14:16:51 +0530

    arrenged the files

--- 1st sept/js.js
+++
@@ -1,8 +0,0 @@
-let array=["anand","baldev","Rathod","anand"]
-let array2=[1,2,3,4,5]
-// array.find("anand")
-array2.sort()
-console.log(array)
-console.log(array2)
-console.log(array.indexOf("anand"))
-console.log(array.lastIndexOf("anand"))
